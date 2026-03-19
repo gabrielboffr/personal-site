c04c2413abaaf5e0293daabea5767b427be2da58
Gabriel Boff Raupp <gabrielbraupp@gmail.com>
2026-03-19 14:16:12 -0300

    updates

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -43,28 +43,41 @@ const Home = () => {
   ];
 
   return (
-    <div className="flex items-center justify-center bg-transparent text-slate-100 p-4 gap-12 h-[calc(100vh-56px)]">
+    <div className="flex items-center justify-center bg-transparent text-slate-100 px-24 gap-12 h-[calc(100vh-64px)]">
       <div className="flex flex-col gap-4">
-        <div className="flex flex-col items-center gap-2">
-          <h1 className="text-6xl">Gabriel Boff Raupp</h1>
-          <h2 className="text-2xl">Desenvolvedor Web</h2>
-          <p className="text-sm text-center w-10/12">
-            Construo aplicações web modernas utilizando React, TypeScript e
-            Javascript, com foco em criar soluções úteis, bem estruturadas e
-            fáceis de manter
-          </p>
-        </div>
-        <div className="flex gap-8 w-1/3 m-auto">
-          <button className="bg-indigo-500 p-2 rounded-xl w-full">
-            <a href="https://linkedin.com/in/gabrielboffr">LinkedIn</a>
-          </button>
-          <button className="bg-indigo-500 p-2 rounded-xl w-full">
-            <a href="https://github.com/gabrielboffr">GitHub</a>
-          </button>
+        <div className="flex flex-col gap-2">
+          <h1 className="text-6xl font-bold">
+            Gabriel{" "}
+            <span className="bg-clip-text bg-linear-to-r from-blue-400 to-blue-900 text-transparent">
+              Boff Raupp
+            </span>
+          </h1>
+          <h2 className="text-2xl font-medium">Desenvolvedor Web</h2>
+          <div className="flex flex-col w-9/12 gap-4">
+            <p className="text-sm font-light text-center">
+              Construo aplicações web modernas utilizando React, TypeScript e
+              Javascript, com foco em criar soluções úteis, bem estruturadas e
+              fáceis de manter
+            </p>
+            <div className="flex gap-8 w-1/3 items-center justify-center mx-auto">
+              <a
+                href="https://linkedin.com/in/gabrielboffr"
+                className="bg-indigo-500 px-4 py-2 rounded-xl w-full text-center shadow-glow"
+              >
+                LinkedIn
+              </a>
+              <a
+                href="https://github.com/gabrielboffr"
+                className="bg-indigo-500 px-4 py-2 rounded-xl w-full text-center shadow-glow duration-300"
+              >
+                GitHub
+              </a>
+            </div>
+          </div>
         </div>
       </div>
       <div className="relative rounded-3xl border border-white/10 bg-[#050505]/80 overflow-hidden shadow-2xl group">
-        <div className="flex justify-between h-8 bg-surface p-3">
+        <div className="flex justify-between h-8 bg-surface p-6">
           <div className="flex gap-2 items-center">
             <div className="rounded-full h-3 w-3 bg-red-400"></div>
             <div className="rounded-full h-3 w-3 bg-yellow-400"></div>
@@ -76,11 +89,11 @@ const Home = () => {
           </div>
         </div>
         <code>
-          <div className="bg-surface p-3">
+          <div className="bg-background p-3">
             {code.map((line, i) => (
               <div key={i} className="flex">
-                <div className="w-6 text-right mr-4 line-number select-none italic">
-                  {i + 1}
+                <div className="pl-4 text-right mr-6 line-number select-none italic opacity-20">
+                  0{i + 1}
                 </div>
                 <div>
                   {line.map((token, j) => (
